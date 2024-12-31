cad569466ef5f82fdf1442cf3329613a49fc6160
avearmin <arminradoncic1@gmail.com>
2024-12-30 22:56:30 -0500

    Add support for parsing string key and value pairs

--- lib/parser.ex
+++ lib/parser.ex
@@ -1,17 +1,12 @@
-defmodule Parser do
-  def parse([:lcurly | tail]) do
-    parse_object(%{}, tail)
-  end
+defmodule Parser do 
+  def parse([:lcurly | tail]), do: parse_object(%{}, tail)
+  
+  defp parse_object(acc, [:rcurly | _tail]), do: {:ok, acc}
+  defp parse_object(acc, [:comma | tail]), do: parse_object(acc, tail)
 
-  defp parse_object(acc, [:rcurly | tail]) do
-    case tail do
-      [:eof] -> {:ok, acc}
-      _ ->  [{:ok, acc} | parse(tail)] 
-    end
-  end
-
-  defp parse_object(_acc, [:eof | _tail]) do
-    [{:error, "unexpected eof when expecting `}`"}]
+  defp parse_object(acc, [{:string, key}, :colon, {:string, value} | tail]) do
+    new_acc = Map.put(acc, key, value)
+    parse_object(new_acc, tail)
   end
 end
 
--- test/parser_test.exs
+++ test/parser_test.exs
@@ -5,6 +5,12 @@ defmodule ParserTest do
   test "empty object" do
     tokens = Lexer.lex([], "{}")
 
-    assert Parser.parse(tokens) == {:ok, %{}} 
+    assert Parser.parse(tokens) == {:ok, %{}}
+  end
+
+  test "object with 3 key value pairs" do
+    tokens = Lexer.lex([], "{\"foo\":\"foo\",\"bar\":\"bar\",\"baz\":\"baz\"}")
+    
+    assert Parser.parse(tokens) == {:ok, %{"foo" => "foo", "bar" => "bar", "baz" => "baz"}}
   end
 end
